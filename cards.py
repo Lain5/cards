5a31d63873cc780ce44e1fd366c21810f951fd3d
Lain5 <thewirediwakura@gmail.com>
2019-09-15 18:33:17 -0400

    check straight, flush, straight flush now working

--- cards.py
+++ cards.py
@@ -35,51 +35,119 @@ class Card(object):
         return suit_dict[self.suit]
 
 
-# Populate the deck with 52 card objects
+# Populate the deck with 52 card objects and shuffles them
 deck = []
-
+p1_hand = []
+p2_hand = []
 
 def populate():
     for i in range(1, 5):
         for j in range(1, 14):
             deck.append(Card(j, i))
 
-
 populate()
+#shuffle(deck)
 
 # Deals five cards to player 1 and player 2
-p1_hand = []
-p2_hand = []
-
 
 def deal(x):
-    print("Your hand is:")
+    print("Your hand is...")
     for i in range(x):
         p1_hand.append(deck.pop(0))
-        p2_hand.append(deck.pop(0))
         print(
             f"{p1_hand[i].translate_rank()} of {p1_hand[i].translate_suit()}")
+    print("\n")
 
-# Reveals the top # cards of the deck
+    print("Your opponent's hand is...")
+    for i in range(x):
+        p2_hand.append(deck.pop(0))
+        print(
+            f"{p2_hand[i].translate_rank()} of {p2_hand[i].translate_suit()}")
+
+deal(5)
+
+def check_straight_flush(hand):
+    if check_flush(hand) and check_straight(hand):
+        return True
+    else:
+        return False
+
+def check_quads(hand):
+    ranks = {1:0, 
+             2:0,
+             3:0,
+             4:0,
+             5:0, 
+             6:0,
+             7:0,
+             8:0,
+             9:0,
+             10:0,
+             11:0,
+             12:0,
+             13:0}
+             
+    for i in range(1,14):
+        for j in range (0,5):
+            if hand[j].rank == i:
+                ranks[j] += 1
+    print(ranks)
+    
+check_quads(p1_hand)
+
+# checks to see if the hand is a value by adding the suit of each card in the hand to a set (which only accepts unique values). 
+# If length of set = 1, flush = true.
+
+def check_flush(hand):
+    suits = set()
+    for i in range(len(hand)):
+        suits.add(hand[i].suit)
+    if len(suits) == 1:
+        return True
+    else:
+        return False
+
+# checks to see if the hand is a straight by adding the rank of each card in the set to a list, which is sorted in ascending order.
+# if the value of the rank of the highest card - the lowest card = 4, then straight = true
+
+def check_straight(hand):
+    ranks = []
+    for i in range(len(hand)):
+        ranks.append(hand[i].rank)
+    ranks.sort()
+    if ranks[4] - ranks[0] == 4:
+        return True
+    else:
+        return False
+
+print(check_straight_flush(p1_hand))
 
 
-print("""
-      Welcome to Five Card Stud! Your options are:
-      showdown - evaluate the winner of the game
-      deal - deal five cards to yourself an your opponent
-      shuffle - randomize the deck
-      peek # - output # cards of cards from the top of the deck
-      """)
+# def evaluate(hand):
+#     if 
+#     straight flush
+#     if
+#     quads
+#     if
+#     full house
+#     if
+#     flush
+#     if
+#     straight
+#     if
+#     trips
+#     if
+#     two pair
+#     if
+#     one pair
+#     if 
+#     high card
 
-choice = input(">")
+#     then
+#     establish strength within hand classification
 
-if choice == "shuffle":
-    shuffle(deck)
-elif choice == "deal":
-    deal(5)
-
-print('stop')
-print('no really')
-
-# elif choice == "peek":
-#     for i in range(5):
+#     then
+#     compare the strength of the two players
+    
+#     then
+#     print the hand classification of player 1 and player 2, and declare winner based on strength
