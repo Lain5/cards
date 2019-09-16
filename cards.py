a88618391af6975c1bd8c5664c22f03e2c12993c
Lain5 <thewirediwakura@gmail.com>
2019-09-15 23:17:39 -0400

    hand classifications

--- cards.py
+++ cards.py
@@ -1,5 +1,5 @@
 from random import shuffle
-
+from collections import defaultdict
 
 class Card(object):
 
@@ -46,7 +46,7 @@ def populate():
             deck.append(Card(j, i))
 
 populate()
-#shuffle(deck)
+shuffle(deck)
 
 # Deals five cards to player 1 and player 2
 
@@ -66,34 +66,39 @@ def deal(x):
 
 deal(5)
 
+# Straight Flush: checks to see if the hand clears the tests for both straight and flush
+
 def check_straight_flush(hand):
     if check_flush(hand) and check_straight(hand):
         return True
     else:
         return False
 
+# Quads: Uses the defaultdict object to count the number of instances of each rank in the hand. If you have 4x of one rank, True.
+
 def check_quads(hand):
-    ranks = {1:0, 
-             2:0,
-             3:0,
-             4:0,
-             5:0, 
-             6:0,
-             7:0,
-             8:0,
-             9:0,
-             10:0,
-             11:0,
-             12:0,
-             13:0}
-             
-    for i in range(1,14):
-        for j in range (0,5):
-            if hand[j].rank == i:
-                ranks[j] += 1
-    print(ranks)
+    ranks = []
+    for i in range(len(hand)): 
+        ranks.append(hand[i].rank)
+    rank_count = defaultdict(int)
+    for j in ranks:
+        rank_count[j] += 1
+    if sorted(rank_count.values()) == [1,4]:
+        return True
+    return False
     
-check_quads(p1_hand)
+# Full House: Uses the defaultdict object to count the number of instances of each rank in the hand. If you have 3x of one rank and 2x of another, True.
+
+def check_boat(hand):
+    ranks = []
+    for i in range(len(hand)): 
+        ranks.append(hand[i].rank)
+    rank_count = defaultdict(int)
+    for j in ranks:
+        rank_count[j] += 1
+    if sorted(rank_count.values()) == [2,3]:
+        return True
+    return False
 
 # checks to see if the hand is a value by adding the suit of each card in the hand to a set (which only accepts unique values). 
 # If length of set = 1, flush = true.
@@ -118,30 +123,72 @@ def check_straight(hand):
     if ranks[4] - ranks[0] == 4:
         return True
     else:
+# checks for the wheel
+        if ranks == [1,2,3,4,13]:
+            return True
         return False
 
-print(check_straight_flush(p1_hand))
+# Trips: Uses the defaultdict object to count the number of instances of each rank in the hand. If you have 3x of one rank and 1x of two other ranks, True.
 
+def check_trips(hand):
+    ranks = []
+    for i in range(len(hand)): 
+        ranks.append(hand[i].rank)
+    rank_count = defaultdict(int)
+    for j in ranks:
+        rank_count[j] += 1
+    if sorted(rank_count.values()) == [1,1,3]:
+        return True
+    return False
 
-# def evaluate(hand):
-#     if 
-#     straight flush
-#     if
-#     quads
-#     if
-#     full house
-#     if
-#     flush
-#     if
-#     straight
-#     if
-#     trips
-#     if
-#     two pair
-#     if
-#     one pair
-#     if 
-#     high card
+# Two Pair: Uses the defaultdict object to count the number of instances of each rank in the hand. If you have 2x of one rank and 2x of another, True.
+
+def check_two_pair(hand):
+    ranks = []
+    for i in range(len(hand)): 
+        ranks.append(hand[i].rank)
+    rank_count = defaultdict(int)
+    for j in ranks:
+        rank_count[j] += 1
+    if sorted(rank_count.values()) == [1,2,2]:
+        return True
+    return False
+
+# One Pair: Uses the defaultdict object to count the number of instances of each rank in the hand. If you have 2x of one rank and 3 others, True.
+
+def check_one_pair(hand):
+    ranks = []
+    for i in range(len(hand)): 
+        ranks.append(hand[i].rank)
+    rank_count = defaultdict(int)
+    for j in ranks:
+        rank_count[j] += 1
+    if sorted(rank_count.values()) == [1,1,1,2]:
+        return True
+    return False
+
+def assign_hand_classification(hand):
+    if check_straight_flush(hand):
+        return 9
+    if check_quads(hand):
+        return 8
+    if check_boat(hand):
+        return 7
+    if check_flush(hand):
+        return 6
+    if check_straight(hand):
+        return 5
+    if check_trips(hand):
+        return 4
+    if check_two_pair(hand):
+        return 3
+    if check_one_pair(hand):
+        return 2
+    else:
+        return 1
+
+print(assign_hand_classification(p1_hand))
+print(assign_hand_classification(p2_hand))
 
 #     then
 #     establish strength within hand classification
